6e6a2c9ac467d12ea46433f7274ba93901f53bbe
Tran Duc Hai <quanghai.dt4@gmail.com>
2017-09-28 14:22:00 +0700

    Ref

--- public/xuly.js
+++ public/xuly.js
@@ -1,18 +1,32 @@
 var UserComponent = React.createClass({
-    layThongTin: function(){
+    layThongTin: function () {
         alert(this.props.children);
+    },
+    getInitialState() {
+        return { age: parseInt(this.props.tuoi) };
+    },
+    addStudent() {
+        this.state.age = this.state.age + 1;
+        this.setState(this.state);
     },
     render: function () {
         return (
             <div>
                 <h1 className="mauvang">{this.props.ten} - {this.props.ho} - {this.props.children}</h1>
-                <button onClick={()=>{getName(this.props.ho)}}>Detail</button>
+                <h2>{this.state.age}</h2>
+                <button onClick={() => {
+                    var str = this.props.ho + this.props.ten + this.props.children;
+                    getName(str);
+                }}>
+                    Detail
+                </button>
+                <button onClick={this.addStudent}>Increase</button>
             </div>
         )
     }
 })
 
-function getName(ten){
+function getName(ten) {
     alert(ten);
 }
 
@@ -24,10 +38,31 @@ var ClassComponent = React.createClass({
     }
 });
 
+var InputTag = React.createClass({
+    show() {
+        var text = this.refs.sl;
+        alert(text);
+    },
+    render() {
+        return (
+            <div>
+                <select ref="sl">
+                    <option value ="a">AAA</option>
+                    <option value ="b">BBB</option>
+                    <option value ="c">CCC</option>
+                </select>
+                <input type="text" ref="txt" />
+                <button onClick={this.show}>Hien thi</button>
+            </div>
+        )
+    }
+})
+
 
 ReactDOM.render(
     <div>
-        <UserComponent ten="Hai" ho="Tran" >dep chai</UserComponent>
+        <InputTag />
+        <UserComponent ten="Hai" ho="Tran" tuoi="100">dep chai</UserComponent>
     </div>,
 
     document.getElementById("root"));
